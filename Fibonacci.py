de331dfaa7fe14afa34c12e77c98960a11ef9c0c
mattjm100 <mattjm100@hotmail.com>
2018-12-15 14:35:27 +0000

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 12 14:15:20 2018
+
+@author: 41505
+"""
+
+def sum_fib_even(number):
+    for a in 4000000:
+    
+          
+        return number+1+2+3+5
+
+print(sum_fib_even(number))
